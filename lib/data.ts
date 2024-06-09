fea187ddeb78f0d0e30362a31f497268e6661019
Gobind-Barick <152778408+Gobind-Barick@users.noreply.github.com>
2024-06-09 11:59:16 +0530

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -35,52 +35,29 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Mobile Application Developer",
+    location: "Work From Home",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I am working as Mobile Application Developer in Recruit NXT  .",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2024",
   },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
-  },
-] as const;
+ ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Multiple Disease Prediction System",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      "I have developed a multiple disease prediction system by using java, streamlit, android studio",
+    tags: [ "Java", "Flutter", "Android Studio", "StreamLit"],
     imageUrl: corpcommentImg,
   },
   {
-    title: "rmtDev",
+    title: "Weather App",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+      "It helps in prediction of weather",
+    tags: ["Java", "Flutter", "Android Studio"],
     imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
   },
 ] as const;
 
@@ -88,20 +65,13 @@ export const skillsData = [
   "HTML",
   "CSS",
   "JavaScript",
-  "TypeScript",
+  "Android Studio",
   "React",
   "Next.js",
   "Node.js",
   "Git",
-  "Tailwind",
-  "Prisma",
+  "Github",
+  "Flutter",
   "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
-] as const;
+  
+] as const;

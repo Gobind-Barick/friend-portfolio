ae7852a6f825631190e77eb7cd3313804aa5f509
Gobind-Barick <152778408+Gobind-Barick@users.noreply.github.com>
2024-06-09 11:49:13 +0530

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,7 +33,7 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
+        <a className="underline" href="khyatijha2511@gmail.com">
           khyatijha2511@gmail.com
         </a>{" "}
         or through this form.
